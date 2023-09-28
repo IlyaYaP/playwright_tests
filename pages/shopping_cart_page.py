53a8acb1fc325947462c7e364bdfd4ffc585bcb6
Илья <socspec@yandex.ru>
2023-09-28 22:09:12 +0300

    pytest param

--- pages/shopping_cart_page.py
+++ pages/shopping_cart_page.py
@@ -5,8 +5,7 @@ import components
 
 class ShoppingCartPage:
 
-    _CATEGORY_DESKTOP_LOCATOR = "h2 a[title='Show products in category Desktops']"
+    # _CATEGORY_DESKTOP_LOCATOR = "h2 a[title='Show products in category Desktops']"
 
     def open_categories(self, page, category_locator) -> None:
-        components.top_menu.open_computers(page)
         pages.base_page.click(page, category_locator)
--- tests/test_shopping_cart_page.py
+++ tests/test_shopping_cart_page.py
@@ -1,12 +1,16 @@
 import pages
+import components
 import pytest
 import time
 
 
 class TestShoppingCartPage:
 
+    _CATEGORY_DESKTOP_LOCATOR = "h2 a[title='Show products in category Desktops']"
+    
     @pytest.mark.shopping_cart_test()
-    def test_shopping_cart(self, page, category_locator):
+    def test_shopping_cart(self, page):
         pages.index_page.open_index_page(page)
-        pages.shopping_cart_page.open_categories(page, category_locator)
+        components.top_menu.open_computers(page)
+        pages.shopping_cart_page.open_categories(page, self._CATEGORY_DESKTOP_LOCATOR)
         time.sleep(5)
